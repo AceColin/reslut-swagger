fdb7fe4c93d758c8c94d2043f143682b73a7bc45
AceColin <acechenlin@163.com>
2021-07-01 23:55:50 +0800

    #init

---
+++ src/main/java/com/ace/reslutswagger/ReslutSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.ace.reslutswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReslutSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReslutSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ace/reslutswagger/ReslutSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.reslutswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReslutSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
